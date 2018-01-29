15fee93d300e95bd229f7d5a4a578887b92bbcdf
khan99 <ksaadii@gmail.com>
2018-01-29 16:06:18 +0530

    Change Test

---
+++ MyIAM/src/test/SearchTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+import controller.BaseTest;
+
+public class SearchTest extends BaseTest {
+
+	
+	@Test(priority=1)
+	public void setUrl()
+	{
+	 System.out.println("Test Commit");	
+	}
+	
+}
